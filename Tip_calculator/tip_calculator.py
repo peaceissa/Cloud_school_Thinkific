3a919c8a5fc1f91664d9e5d84ca7f6486946fd7b
Peace Love Issa <peaceissa695@gmail.com>
2024-01-02 23:12:01 +0300

    wrote the code for the tip calculator

---
+++ Tip_calculator/tip_calculator.py
@@ -0,0 +1,20 @@
+percent_tip = 0.18;
+percent_tax  = 0.07;
+
+print("Welcome To Asanka Hotel.");
+print("Please Enter Your Bill Amount.");
+bill = float(input());
+print("Please Enter Number Of People")
+people = int(input());
+
+tip = bill * percent_tip;
+Sales_tax = bill * percent_tax;
+
+total = bill + tip + Sales_tax;
+each = total / people;
+print("Tip: ", tip);
+print("Sales Tax: ", Sales_tax);
+print("Your Total Bill Is: ", total);
+print("Each Person Should Pay");
+print(each);
+print("Thank You For Visiting Asanka Hotel");
