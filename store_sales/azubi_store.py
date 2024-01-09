5f9ff232cbd8fa52362752a0197b9baefa94fd25
Peace Love Issa <peaceissa695@gmail.com>
2024-01-09 21:13:51 +0300

    added functionality for products under 30

--- store_sales/azubi_store.py
+++ store_sales/azubi_store.py
@@ -8,16 +8,19 @@ average_price = 0;
 total_revenue = 0;
 total_price = 0;
 average_daily_revenue = 0;
+new_price_list = [];
 
 for i in range(len(prices)):
     total_price += prices[i];
     average_price = total_price / len(prices);
     total_revenue += prices[i] * last_week[i];
+    new_price_list.append(prices[i] - 5);
 
 average_daily_revenue = total_revenue / 7;
 
 print("Average Price: " + str(average_price));
 print("Total Revenue: " + str(total_revenue));
 print("Average Daily Revenue: " + str(average_daily_revenue));
+print("New Price List: " + str(new_price_list));
 
 
